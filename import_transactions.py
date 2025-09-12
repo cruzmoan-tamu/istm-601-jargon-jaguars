aab8ab0e18b27e1e52caf2b03657a659050d05e5
blackshearmaggie <158242318+blackshearmaggie@users.noreply.github.com>
2025-09-12 15:39:03 -0500

    Update import_transactions.py

--- import_transactions.py
+++ import_transactions.py
@@ -1,56 +1,57 @@
-from datetime import datetime
-import re
+from datetime import datetime #used to check date is valid
+import re #checks date matches the yyyy-mm-dd pattern
 
 def main():
     
     def date():
-        pattern = r"^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$"
+        pattern = r"^\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$" #year is 4 digits, month is between 01-12, day is between 01-31
         
         while True:
-            date = input("Enter transaction date (yyyy-mm-dd): ")
+            date = input("Enter transaction date (yyyy-mm-dd): ") #get date from user
             
             if not re.match(pattern,date):
-                print("Invalid format. Please enter date in yyyy-mm-dd format.")
+                print("Invalid format. Please enter date in yyyy-mm-dd format.") #states that the input was invalid
                 continue
             
             try:
-                datetime.strptime(date, "%Y-%m-%d")
-                print("Date entered:", date)
+                datetime.strptime(date, "%Y-%m-%d") #checks that the date exists
+                print("Date entered:", date) #confirms the date entered
                 return date
             except ValueError:
-                print("Invalid date! That day doesn't exist. Please try again.")
+                print("Invalid date! That day doesn't exist. Please try again.") #states that the date was invalid
                 continue
             
     def description():
         while True:
-            description = input("Enter transaction description: ")
+            description = input("Enter transaction description: ") #gets the description
             
-            if len(description) == 0:
+            if len(description) == 0: #description cannot be blank
                 print("Description cannot be empty")
                 continue
-            elif len(description) > 75:
+            elif len(description) > 75: #description is limited to 75 characters
                 print("Invalid! Too many characters.")
                 continue
             else:
-                print("Description entered:", description)
+                print("Description entered:", description) #state the description entered
                 return description
             
             
     def amount():
          while True:
-             amount = input("Enter transaction amount: ")
+             amount = input("Enter transaction amount: ") #gets the amount from user
              
              try:
-                 value = float(amount)
-                 if value < 0:
+                 value = float(amount) #converts to float
+                 if value < 0: #amount cannot be negative
                      print("Amount cannot be negative.")
                      continue
-                 print("Amount entered:", f"{value:.2f}")
+                 print("Amount entered:", f"{value:.2f}") #adds 2 decimal points for whole numbers and confirms amount
                  return value 
              except ValueError:
-                 print("Invalid input. Please enter numbers only.") 
+                 print("Invalid input. Please enter numbers only.") #Invalid input
                  
     def category():
+        #categories dictionary
         categories = {
             "1": "Living Expenses",
             "2": "Food and Dining",
@@ -61,6 +62,7 @@ def main():
             "7": "Earned Income",
             "8": "Unearned Income"
         }
+        #user chooses category
         while True:
             category = input(
                 "Enter category by number:\n"
@@ -76,43 +78,31 @@ def main():
             ).strip()
             
             if category in categories:
-                print("Category selected: ", categories[category])
+                print("Category selected: ", categories[category]) #confirm category selected
                 return categories[category]
             else:
-                print("Invalid input! Please enter a number 1 to 8.")
-   
-    def amount():
-         while True:
-             amount = input("Enter transaction amount: ")
-             
-             try:
-                 value = float(amount)
-                 if value < 0:
-                     print("Amount cannot be negative.")
-                     continue
-                 print("Amount entered:", f"{value:.2f}")
-                 return value 
-             except ValueError:
-                 print("Invalid input. Please enter numbers only.")       
-    
+                print("Invalid input! Please enter a number 1 to 8.")    #user did not enter number between 1-8
     
     def typed():
+    #types dictionary
         types = {
             "1": "Income",
             "2": "Expense",
         }
+
+        #user chooses type
         while True:
             typed = input(
-                "Enter category by number:\n"
+                "Enter Type by number:\n"
                 "1 - Income\n"
                 "2 - Expense\n"
             ).strip()
             
             if typed in types:
-                print("Category selected: ", types[typed])
+                print("Type selected: ", types[typed])
                 return types[typed]
             else:
-                print("Invalid input! Please enter a number 1 or 2.")
+                print("Invalid input! Please enter a number 1 or 2.") #user did not enter numbers 1 or 2
                 
     while True:
         date_value = date()  
@@ -145,4 +135,5 @@ if __name__ == "__main__":
     
 
     
+
 
